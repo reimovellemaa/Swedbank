a3c0d3f97338a4a42e739fbbf6a357e88f928d87
Thomas-Bairam Toodo <thomastoodo@msn.com>
2017-04-03 00:16:56 +0300

    rest for new methods and fixed classpath

--- src/main/java/com/swed/AndmedResource.java
+++ src/main/java/com/swed/AndmedResource.java
@@ -1,5 +1,7 @@
 package com.swed;
 
+import java.sql.SQLException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,6 +15,33 @@ public class AndmedResource {
 	@Produces("text/plain")
 	public String getStrink() {
 		return dao.getAndmed();
+	}
+	
+	
+	//Meetodeid saab callida 
+	//localhost:8080/Dashboard/rest/andmed/getAllMeasureCnt
+	//localhost:8080/Dashboard/rest/andmed/getCompletness
+	//localhost:8080/Dashboard/rest/andmed/getKPI
+	
+	@Path("getAllMeasuresCnt")
+	@GET
+	@Produces("text/plain")
+	public String getAllMeasuresCnt() throws SQLException{
+		return dao.getAllMeasuresCnt("foo", "bar");
+	}
+	
+	@Path("getCompletness")
+	@GET
+	@Produces("text/plain")
+	public String getCompletness() throws SQLException{
+		return dao.getCompletness("foo", "bar");
+	}
+	
+	@Path("getKPI")
+	@GET
+	@Produces("text/plain")
+	public String getKPI() throws SQLException{
+		return dao.getKPI("foo", "bar");
 	}
 
 	
