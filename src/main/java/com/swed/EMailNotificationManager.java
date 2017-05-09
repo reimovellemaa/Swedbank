ca6146d3266da5c6ae1a0c6be37256bfcd876621
Reimo.Vellemaa <reimo.vellemaa@ttu.ee>
2017-05-09 21:54:31 +0300

    fix import

--- src/main/java/com/swed/EMailNotificationManager.java
+++ src/main/java/com/swed/EMailNotificationManager.java
@@ -1,22 +1,14 @@
 package com.swed;
 import javax.mail.Authenticator;
-import java.net.PasswordAuthentication;
+import javax.mail.PasswordAuthentication;
 import java.util.Date;
 import java.util.Properties;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.BodyPart;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 
 public class EMailNotificationManager {
